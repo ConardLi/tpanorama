315611a51666869a2a178ba43054c97097bfc39b
shiqi.li <1009903985@qq.com>
2018-12-17 21:27:43 +0800

    fix:rm unuse

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ var logger = require('morgan');
 var cons = require('consolidate')
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 
 var app = express();
 
@@ -24,7 +23,6 @@ app.use('/img', express.static(path.join(__dirname, '/img')));
 app.use(express.static(path.join(__dirname, 'views')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
